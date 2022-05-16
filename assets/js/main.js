25a375b6c64c8043aba19ed1b5e1355c598ecfbf
ShanchaiZ <shanchai.zahid@gmail.com>
2022-05-16 17:36:54 -0400

    added skill section with dummy HTML data, added css for bar design that reflect html skills, and added added accordian feature to skills

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,7 +7,7 @@ const navMenu = document.getElementById("nav-menu"),
 /* ==================== MENU SHOW ==================== */
 // validate if constant exists //
 
-if(navToggle){
+if (navToggle) {
     navToggle.addEventListener("click", () => {
         navMenu.classList.add("show-menu")
     })
@@ -17,7 +17,7 @@ if(navToggle){
 /* ==================== MENU HIDDEN ==================== */
 // validate if constant exists //
 
-if(navClose){
+if (navClose) {
     navClose.addEventListener("click", () => {
         navMenu.classList.remove("show-menu")
     })
@@ -27,17 +27,37 @@ if(navClose){
 /* ==================== REMOVE MENU MOBILE ==================== */
 const navLink = document.querySelectorAll(".nav__link")
 
-function linkAction(){
+function linkAction() {
     const navMenu = document.getElementById("nav-menu")
     // When we click on each nav__link, we remove show-menu class
     navMenu.classList.remove("show-menu")
 }
 
-navLink.forEach(n => n.addEventListener("click" , linkAction))
+navLink.forEach(n => n.addEventListener("click", linkAction))
 
 
 
 /* ==================== ACCORDION SKILLS ==================== */
+const skillsContent = document.getElementsByClassName("skills__content"),
+    skillsHeader = document.querySelectorAll(".skills__header")
+
+
+function toggleSkills(){
+    let itemClass = this.parentNode.className
+
+    for(i = 0; i < skillsContent.length; i++){
+        skillsContent[i].className = "skills__content skills__close"
+    }
+    if(itemClass === "skills__content skills__close"){
+        this.parentNode.className = "skills__content skills__open"
+    }
+}
+
+skillsHeader.forEach((el) => {
+    el.addEventListener("click" , toggleSkills)
+})
+
+
 
 /* ==================== QUALIFICATIONS TAB ==================== */
 
