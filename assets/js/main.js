4a8bda9ed4bd124d95b8cf1c09ca416bfc9719a8
ShanchaiZ <shanchai.zahid@gmail.com>
2022-05-19 16:02:01 -0400

    added qualification button clicking logic for education/work where clicking on a button hides the other option in main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -42,25 +42,43 @@ const skillsContent = document.getElementsByClassName("skills__content"),
     skillsHeader = document.querySelectorAll(".skills__header")
 
 
-function toggleSkills(){
+function toggleSkills() {
     let itemClass = this.parentNode.className
 
-    for(i = 0; i < skillsContent.length; i++){
+    for (i = 0; i < skillsContent.length; i++) {
         skillsContent[i].className = "skills__content skills__close"
     }
-    if(itemClass === "skills__content skills__close"){
+    if (itemClass === "skills__content skills__close") {
         this.parentNode.className = "skills__content skills__open"
     }
 }
 
 skillsHeader.forEach((el) => {
-    el.addEventListener("click" , toggleSkills)
+    el.addEventListener("click", toggleSkills)
 })
 
 
 
 /* ==================== QUALIFICATIONS TAB ==================== */
+const tabs = document.querySelectorAll("[data-target]"),
+    tabContents = document.querySelectorAll("[data-content]")
 
+
+tabs.forEach(tab =>{
+    tab.addEventListener("click", () => {
+        const target = document.querySelector(tab.dataset.target)
+
+        tabContents.forEach(tabContent => {
+            tabContent.classList.remove("qualification__active")
+        })
+        target.classList.add("qualification__active")
+
+        tabs.forEach(tab => {
+            tab.classList.remove("qualification__active")
+        })
+        tab.classList.add("qualification__active")
+    })
+})
 
 
 /* ==================== SERVICES MODAL ==================== */
