8dc874ad50c02761d3b5b09e077f5c9d4032b3c8
ShanchaiZ <shanchai.zahid@gmail.com>
2022-06-02 14:24:54 -0400

    added shadow to nav bar header when nav bar minimized feature using js and css

--- assets/js/main.js
+++ assets/js/main.js
@@ -171,6 +171,16 @@ window.addEventListener('scroll', scrollActive)
 
 
 /* ==================== CHANGE BACKGROUND HEADER ==================== */
+function scrollHeader(){
+    const nav = document.getElementById("header")
+
+    if(this.scrollY >= 80) nav.classList.add("scroll-header"); 
+    else nav.classList.remove("scroll-header")
+}
+
+window.addEventListener("scroll", scrollHeader)
+
+
 /* ==================== SHOW SCROLL TOP ==================== */
 /* ==================== DARK LIGHT HERE ==================== */
 
